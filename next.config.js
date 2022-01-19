25ca76c872c1f83479bf2c5067126961050dc4dc
mrrs878 <mrrs878@foxmail.com>
2022-01-19 21:02:32 +0800

    build: 调整basePath

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 1985-10-26 16:15:00
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2022-01-19 20:19:03
+ * @LastEditTime: 2022-01-19 21:00:24
  */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/next-blog',
   images: {
     domains: ["blog.mrrs.top"],
   },
