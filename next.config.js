9ebe218da0537d915531689bdf63fa6217b09280
mrrs878 <mrrs878@foxmail.com>
2022-05-12 22:45:06 +0800

    chore: 修改静态资源地址

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 1985-10-26 16:15:00
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2022-01-21 11:03:35
+ * @LastEditTime: 2022-05-12 22:43:41
  */
 
 module.exports = {
   reactStrictMode: true,
-  basePath: '/next-blog',
   images: {
     domains: ['blog.mrrs.top'],
   },
