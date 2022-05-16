ba7a2ad0aa75b4888ae5e5c2c0029d43248f5b83
mrrs878 <mrrs878@foxmail.com>
2022-05-16 22:49:31 +0800

    chore: 修改icon

---
+++ pages/_document.jsx
@@ -0,0 +1,24 @@
+/*
+ * @Author: mrrs878@foxmail.com
+ * @Date: 2022-05-16 22:46:35
+ * @LastEditors: mrrs878@foxmail.com
+ * @LastEditTime: 2022-05-16 22:48:36
+ */
+
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="https://mrrsblog.oss-cn-shanghai.aliyuncs.com/avatar.jpg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
