61645c4ef975754c3eb855cd8c4dbdfca672eb18
mrrs878 <mrrs878@foxmail.com>
2022-09-14 22:48:38 +0800

    feat: 去除阅读量显示

--- pages/post/[title].tsx
+++ pages/post/[title].tsx
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2022-01-19 19:25:20
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2022-05-07 11:06:36
+ * @LastEditTime: 2022-09-14 22:48:01
  */
 
 import { FC } from 'react';
@@ -52,13 +52,17 @@ const Post: FC<IPostProps> = ({
               </svg>
               {post.updateDate}
             </span>
-            <span className="flex align-middle">
-              <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 inline-block mr-1">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-              </svg>
-              44
-            </span>
+            {
+              false && (
+                <span className="flex align-middle">
+                  <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 inline-block mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                  </svg>
+                  44
+                </span>
+              )
+            }
           </div>
         </div>
         <div className="mt-8 flex">
